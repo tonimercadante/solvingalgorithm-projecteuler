37100890ad1adb1f29197c06e1efc9b492c135ad
Toni Mercadante <antonio.mercadante@ame-sp.org.br>
2023-09-19 23:00:47 -0300

    brute force method for the Least common multiple, need improvements

--- 5/main.c
+++ 5/main.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 
 int main (void) {
-  
+  int target = 2520;
+  long x = 0;
+  long i = 1;
+
+  while (1) {
+
+      
+      for (int j = 1; j <= 20; j++) {
+        printf("j: %i i: %li \n", j, i);
+        if(i % j != 0) {
+          x = 0;
+          break;
+        }
+
+        x = i;
+      }
+    i++;
+
+
+    if(x != 0) {
+      break;
+    }
+  }
+
+  printf("Valor: %li x: %li", i, x);  
 
   return 0;
 }
