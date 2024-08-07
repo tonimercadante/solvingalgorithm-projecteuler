0e31feb35bb4d734b16da02c0cb165b89cc74129
Lucas Bernardo <100177406+behw0@users.noreply.github.com>
2024-08-07 11:21:00 -0300

    problem 19 resolving with python

--- 19/main.py
+++ 19/main.py
@@ -1,1 +1,10 @@
+# problem_19.py
+import datetime
 
+sundays = 0
+for year in range(1901, 2001):
+    for month in range(1, 13):
+        if datetime.date(year, month, 1).weekday() == 6:  #6 = sunday
+            sundays += 1
+
+print(f"Number of Sundays that fell on the first day of the month during the 20th century: {sundays}")
