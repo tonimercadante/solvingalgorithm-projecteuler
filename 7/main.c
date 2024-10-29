972300787869a15f3b85ba7cd9df726370ae7283
Toni Mercadante <mercadanteneto@gmail.com>
2024-10-28 21:55:56 -0300

    7 - prime

--- 7/main.c
+++ 7/main.c
@@ -1,10 +1,50 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdbool.h>
 
-int main(void) {
-  int prime_position = 0;
-  int res = pow(2,2);  
-  printf("perfect power: %i:%i", 2, res);
+bool is_prime(number)
+{
+  if (number <= 1)
+  {
 
-  return 0;  
+    return false;
+  }
+
+  for (int i = 2; i < number; i++)
+  {
+    if (number % i == 0)
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+int calculate_largest_prime(ceil)
+{
+  int prime_count = 0;
+  int largest_prime = 0;
+
+  int i = 0;
+  while (prime_count < ceil)
+  {
+    if (is_prime(i))
+    {
+      largest_prime = i;
+      prime_count++;
+    }
+
+    i++;
+  };
+
+  return largest_prime;
+}
+
+int main(void)
+{
+  int ceil = 10001;
+
+  int largest_prime = calculate_largest_prime(ceil);
+
+  printf("MAX PRIMEIS: %d", largest_prime);
 }
